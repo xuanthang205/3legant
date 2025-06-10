8a8d2ddbcaabc5d35e14f7551a2f3d72bc434eba
쑤언 탕_Xuan Thang <thangdx@hivelab.com>
2025-06-10 16:56:02 +0700

    Slider/Banner 1

---
+++ js/swiper.js
@@ -0,0 +1,32 @@
+// const swiper = new Swiper('.product_slides', {
+//   slidesPerView: 'auto',
+//   spaceBetween: 16,
+//   touchReleaseOnEdges: true,
+//   breakpoints: {
+//     768: {
+//       spaceBetween: 24,
+//     },
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//     draggable: true,
+//   },
+//   mousewheel: true,
+// });
+
+const swiper = new Swiper('.slider_wrap', {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+});
